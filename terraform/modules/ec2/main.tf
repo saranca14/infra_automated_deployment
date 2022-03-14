e2544af13746aa3b7e19c828de156a9f80735a34
ca14 <ca14@ca14s-MacBook-Air.local>
2022-03-15 00:35:41 +0530

    ansible edit

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -19,7 +19,7 @@ resource "aws_instance" "ec2_public" {
   vpc_security_group_ids      = [var.sg_pub_id]
 
    provisioner "local-exec" {
-        command = "sleep 100; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ubuntu --private-key ./aws_ec2_key.pem -i '${aws_instance.ec2_public.public_ip},' site.yml"
+        command = "sleep 100; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ubuntu --private-key ./aws_ec2_key.pem -i '${aws_instance.ec2_public.public_ip},' ../ansible/site.yml"
     }
 
   tags = {
