52f15f7d69d61987e44f9c87a0f4069978402270
ca14 <ca14@ca14s-MacBook-Air.local>
2022-03-20 19:08:23 +0530

    added some changes

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -8,6 +8,22 @@ data "aws_ami" "ubuntu" {
     owners = ["099720109477"]
 }
 
+resource "tls_private_key" "ec2_private_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+  provisioner "local-exec" {
+    command = "echo '${tls_private_key.ec2_private_key.private_key_pem}' > ~/.ssh/${var.key_name}.pem"
+  }
+}
+
+resource "null_resource" "key-perm" {
+  depends_on = [tls_private_key.ec2_private_key,
+  ]
+  provisioner "local-exec" {
+    command = "chmod 400 ~/.ssh/${var.key_name}.pem"
+  }
+}
+
 // Configure the Nginx EC2 instance in a public subnet
 resource "aws_instance" "ec2_public" {
   ami                         = data.aws_ami.ubuntu.id
--- terraform/modules/ssh-key/main.tf
+++ terraform/modules/ssh-key/main.tf
@@ -17,19 +17,4 @@ resource "aws_key_pair" "key_pair" {
 }
 */
 
-resource "tls_private_key" "ec2_private_key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-  provisioner "local-exec" {
-    command = "echo '${tls_private_key.ec2_private_key.private_key_pem}' > ~/.ssh/${var.key_name}.pem"
-  }
-}
-
-resource "null_resource" "key-perm" {
-  depends_on = [tls_private_key.ec2_private_key,
-  ]
-  provisioner "local-exec" {
-    command = "chmod 400 ~/.ssh/${var.key_name}.pem"
-  }
-}
 
