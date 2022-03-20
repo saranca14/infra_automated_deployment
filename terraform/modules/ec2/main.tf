7267be8c6ab42bcc43f536a9202581735df5a95a
ca14 <ca14@ca14s-MacBook-Air.local>
2022-03-20 19:40:17 +0530

    added some changes

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -47,3 +47,13 @@ resource "aws_instance" "ec2_public" {
     "Name" = "${var.namespace}-nginx_server"
   }
 }
+
+# generate inventory file for Ansible
+resource "local_file" "hosts_cfg" {
+  content = templatefile("${path.module}/hosts.tpl",
+    {
+      server_ip = aws_instance.ec2_public.*.public_ip
+    }
+  )
+  filename = "../ansible/hosts.cfg"
+}
