073219395293d7a7448dc7d1e62e29c90bfb160f
ca14 <ca14@ca14s-MacBook-Air.local>
2022-03-20 19:43:26 +0530

    added some changes

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -52,7 +52,7 @@ resource "aws_instance" "ec2_public" {
 resource "local_file" "hosts_cfg" {
   content = templatefile("${path.module}/hosts.tpl",
     {
-      server_ip = aws_instance.ec2_public.*.public_ip
+      server_ip = aws_instance.ec2_public.public_ip
     }
   )
   filename = "../ansible/hosts.cfg"
